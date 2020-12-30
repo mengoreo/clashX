d22143384d13a645766cddbfa5d60f6f88d5e304
Kenton Wendell <buster.shannon@icloud.com>
2020-12-31 00:53:17 +0800

    removed jquery

--- ClashX Safari Extension/Resources/popup/src/js/main.js
+++ ClashX Safari Extension/Resources/popup/src/js/main.js
@@ -1,29 +1,25 @@
 import { domainOrHost } from "./tld"
-import jQuery from "./jquery-3.5.1.min"
 
 const failedResources = document.getElementById("failedResources");
 const resourceStatusTemplate = document.getElementById("resourceStatusTemplate");
 var failedResourcesCollapsed = false;
 
-document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("openHomePage").addEventListener("click", openHomePage);
-});
 
 function hideAllActionsInModules() {
     let actions = document.getElementsByClassName("actions");
-    for (let i=0; i < actions.length; i++) {
+    for (let i = 0; i < actions.length; i++) {
         actions[i].hidden = true;
     }
 }
 
 function setupOnHoverModule() {
     let moduleContents = document.getElementsByClassName("infoContainer");
-    for (let i=0; i < moduleContents.length; i++) {
+    for (let i = 0; i < moduleContents.length; i++) {
         let module = moduleContents[i];
-        module.onmouseover = function() {
+        module.onmouseover = function () {
             this.getElementsByClassName("actions")[0].hidden = failedResourcesCollapsed;
         };
-        module.onmouseout = function() {
+        module.onmouseout = function () {
             this.getElementsByClassName("actions")[0].hidden = true;
         };
     }
@@ -41,19 +37,19 @@ function buildActions(actions) {
     if (keys.length == 1) {
         console.log("one", keys, values);
         let options = values[0];
-        for (let v=0; v<options.length; v++) {
+        for (let v = 0; v < options.length; v++) {
             let option = document.createElement("option");
             option.value = options[v];
             optionsContainer.appendChild(option);
         }
     } else {
         console.log("many", keys, values);
-        for (let k=0; k<keys.length; k++) {            
+        for (let k = 0; k < keys.length; k++) {
             let optionGroup = document.createElement("optgroup");
             optionGroup.label = keys[k];
 
             let options = values[k];
-            for (let v=0; v<options.length; v++) {
+            for (let v = 0; v < options.length; v++) {
                 let option = document.createElement("option");
                 option.value = options[v];
                 option.text = options[v];
@@ -70,7 +66,7 @@ function insertFaildResource(url) {
     console.log(clone);
     let name = clone.getElementById("resourceName").getElementsByTagName("a")[0];
     name.text = "⚠️ " + domain;
-    
+
     let time = clone.getElementById("resourceTime").getElementsByTagName("a")[0];
     time.text = "Now";
 
@@ -93,28 +89,27 @@ function expandFailedResourcesIfNeeded() {
 
     let modules = document.getElementsByClassName("infoContainer");
     let length = modules.length;
-    
-    for (let i=0; i<length; i++) {
+
+    for (let i = 0; i < length; i++) {
         modules[i].classList.remove("noselect");
         modules[i].removeAttribute("style");
         modules[i].hidden = false;
         modules[i].getElementsByClassName("footer")[0].hidden = true;
     }
 }
+
 function collapseFailedResourcesIfNeeded() {
     if (failedResourcesCollapsed) {
         return;
     }
 
     failedResourcesCollapsed = true;
-    let firstHeight = jQuery(".infoContainer").css( "height" );;
-    
-    let modules = document.getElementsByClassName("infoContainer");
-    let length = modules.length;
+    var firstHeight = document.querySelector(".infoContainer").offsetHeight;
 
+    let modules = document.querySelectorAll(".infoContainer");
+    let length = modules.length;
     if (length > 1) {
-        let firstModule = document.getElementsByClassName("contentArea")[0];
-        firstModule.onmousedown = function() {
+        document.querySelector(".contentArea").onmousedown = function () {
             if (failedResourcesCollapsed) {
                 expandFailedResourcesIfNeeded();
             } else {
@@ -123,30 +118,30 @@ function collapseFailedResourcesIfNeeded() {
         }
     }
 
-    for (let i=0; i<length; i++) {
-        let module = modules[i];
-        module.style.height = firstHeight;
+    failedResourcesCollapsed = length > 2;
+    modules.forEach(function (module, index, parent) {
+        module.style.height = `${firstHeight}px`;
         module.style.position = "absolute";
-        module.style.zIndex = length - i;
-        modules[i].classList.add("noselect");
+        module.style.zIndex = modules.length - index;
+        module.classList.add("noselect");
+        module.querySelector(".footer").hidden = index == 0;
 
-        module.getElementsByClassName("footer")[0].hidden = (i == 0);
+        let scale = 1 - index / 20;
+        let marginTop = 10 * index;
+        console.log("marginTop:", index, marginTop);
+        module.style.transform = `scale(${scale})`;
+        module.style.marginTop = `${marginTop}px`;
 
-        if (i > 2) {
-            module.hidden = true;
-        } else {
-            let scale = 1 - i/20;
-            let marginTop = 10 * i;
-            module.style.transform = `scale(${scale})`;
-            module.style.marginTop = `${marginTop}px`;
+        if (index > 2) {
+            module.style.display = "none";
         }
-
-        failedResourcesCollapsed = i > 2;
-    }
+    })
 }
 
-jQuery(function() {
-    buildActions({"Group1": ["proxy1", "proxy2", "direct"], "Group2": ["proxy1", "proxy2", "direct"]});
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("openHomePage").addEventListener("click", openHomePage);
+    buildActions({ "Group1": ["proxy1", "proxy2", "direct"], "Group2": ["proxy1", "proxy2", "direct"] });
     insertFaildResource("https://github.com/oncletom/tld.js/");
     insertFaildResource("https://wweb.dev/blog/how-to-create-static-website-npm-scripts/wweb.dev/blog/how-to-create-static-website-npm-scripts/wweb.dev/blog/how-to-create-static-website-npm-scripts/wweb.dev/blog/how-to-create-static-website-npm-scripts");
     insertFaildResource("https://github.com/wwebdev/static-website-template");
@@ -160,7 +155,7 @@ jQuery(function() {
 });
 
 function openHomePage() {
-    browser.permissions.request({origins: ['https://www.baidu.com/']}, (granted) => {
+    browser.permissions.request({ origins: ['https://www.baidu.com/'] }, (granted) => {
         if (granted) {
             console.log("Open Home Page Now")
         }
